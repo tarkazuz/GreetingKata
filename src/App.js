ebea3d952716e04910ebab47da1b84db1041619a
Zuzanna Tarka <zuzanna.tarka@signavio.com>
2021-11-26 14:03:49 +0100

    task 2 done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [name, setName] = useState('')
   return <div className="App">My boilerplate
     <div data-testid='myTest'>
-      {`Hello, ${name}`}
+      {`Hello, ${name || 'my friend'}`}
     </div>
     <input type='text' value={name} onChange={e => setName(e.target.value)} />
   
